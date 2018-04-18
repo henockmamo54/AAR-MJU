79e5733731661d4e8d939433a644cf0e7f4a3c5d
DESKTOP-IUH4BPV\Henock <henockmamo54@gmail.com>
2018-04-18 20:44:02 +0900

    project created

---
+++ AAR-MJU-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=aar-mju-af5b-Web-Deploy.pubxml -p:Password=iDinslxPNq4kSYfZkMcrpj3d3A7Kgak3uhtPWMNoC1mcH54J0ZhKrv9yRaXR
+
